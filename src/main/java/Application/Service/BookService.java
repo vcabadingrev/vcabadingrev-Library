db3bbe8e30a30b33a41e7d7143c38efaddb0c477
Virgilio Cabading <vcabadingrev@gmail.com>
2023-12-18 02:32:03 +0000

    implement dao and service for author and book

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -40,7 +40,7 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return null;
+        return this.bookDAO.getAllBooks();
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,15 +51,17 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-
-        return null;
+        if (this.bookDAO.getBookByIsbn(book.getIsbn()) != null) {
+            return null;
+        }
+        return this.bookDAO.insertBook(book);
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return null;
+        return this.bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
